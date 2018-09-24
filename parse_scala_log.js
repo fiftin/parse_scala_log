cf3020d09c2f41f1405cdd7dbebe16af30818611
Denis <denguk@gmail.com>
2018-09-24 23:03:52 +0500

    feat: implemented new format parser

--- parse_scala_log.js
+++ parse_scala_log.js
@@ -5,18 +5,28 @@ function throwUnexpected(c, state) {
   throw new Error('Unexpected character: ' + c + '. State: ' + state);
 }
 
-
-function getObjectByPath(root, path) {
+/**
+ *
+ * @param {object} root
+ * @param {array<string>} path
+ * @param returnLastItemIfArray
+ * @returns {*}
+ */
+function getObjectByPath(root, path, returnLastItemIfArray) {
   let ret = root;
 
   for (const key of path) {
     ret = ret[key];
   }
 
+  if (returnLastItemIfArray && Array.isArray(ret)) {
+    return  ret[ret.length - 1];
+  }
+
   return ret;
 }
 
-module.exports = function(str) {
+function parseOldFormat(str) {
   const ret = {};
   const path = [];
   let state = 'fieldValue';
@@ -129,3 +139,157 @@ module.exports = function(str) {
   }
   return ret.root;
 }
+
+/**
+ * States:
+ *
+ * - fieldName
+ * - fieldWaitValue
+ * - fieldValue
+ * - fieldValueString
+ * - objectBody
+ *
+ * @param str
+ * @returns {*|Node|Element}
+ */
+function parseNewFormat(str) {
+  const ret = {};
+  const path = [];
+  let state = 'objectBody';
+  let token = '';
+  let fieldName = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const c = str[i];
+    switch (c) {
+      case '{':
+        switch (state) {
+          case 'fieldName':
+            let objByPath = getObjectByPath(ret, path)[fieldName];
+            if (objByPath) {
+              if (Array.isArray(objByPath)) {
+                // ok, do nothing
+              } else if (typeof objByPath === 'object') {
+                objByPath = [objByPath];
+                getObjectByPath(ret, path)[fieldName] = objByPath;
+              } else {
+                throw new Error('Only object array supported');
+              }
+              objByPath.push({});
+            } else {
+              getObjectByPath(ret, path)[fieldName] = {};
+            }
+            path.push(fieldName);
+            state = 'objectBody';
+            token = '';
+            fieldName = '';
+            break;
+          case 'fieldValueString':
+            token += c;
+            break;
+          default:
+            throwUnexpected(c, state);
+            break;
+        }
+        break;
+      case '}':
+        switch (state) {
+          case 'objectBody':
+            path.pop();
+            break;
+          case 'fieldValue':
+            getObjectByPath(ret, path, true)[fieldName] = token;
+            fieldName = '';
+            token = '';
+            state = 'objectBody';
+            path.pop();
+            break;
+          case 'fieldValueString':
+            token += c;
+            break;
+          default:
+            throwUnexpected(c, state);
+            break;
+        }
+        break;
+      case ':':
+        switch (state) {
+          case 'fieldName':
+            state = 'fieldWaitValue';
+            break;
+          case 'fieldValueString':
+            token += c;
+            break;
+          default:
+            throwUnexpected(c, state);
+            break;
+        }
+        break;
+      case '"':
+        switch (state) {
+          case 'fieldWaitValue':
+            state = 'fieldValueString';
+            token = '';
+            break;
+          case 'fieldValueString':
+            getObjectByPath(ret, path, true)[fieldName] = token;
+            fieldName = '';
+            token = '';
+            state = 'objectBody';
+            break;
+          default:
+            throwUnexpected(c, state);
+            break;
+        }
+        break;
+      case ' ':
+        switch (state) {
+          case 'fieldValueString':
+            token += c;
+            break;
+          case 'fieldValue':
+            getObjectByPath(ret, path, true)[fieldName] = token;
+            fieldName = '';
+            token = '';
+            state = 'objectBody';
+            break;
+        }
+        break;
+      default:
+        switch (state) {
+          case 'fieldValueString':
+            token += c;
+            break;
+          case 'fieldWaitValue':
+            state = 'fieldValue';
+            token += c;
+            break;
+          case 'objectBody':
+            state = 'fieldName';
+            fieldName += c;
+            break;
+          case 'fieldName':
+            fieldName += c;
+            break;
+          case 'fieldValue':
+            token += c;
+            break;
+        }
+        break;
+    }
+  }
+
+  return ret;
+}
+
+module.exports = function(str, format) {
+  if (!format) {
+    format = 'old';
+  }
+  switch (format) {
+    case 'new':
+      return parseNewFormat(str);
+    case 'old':
+      return parseOldFormat(str);
+  }
+};
--- test/test.js
+++ test/test.js
@@ -7,6 +7,35 @@ describe('parse_scala_log', () => {
     const obj = parse('Car(brand=BMW)');
     expect(obj.brand).to.eq('BMW');
   });
+
+
+
+  it('parse real log 1', () => {
+    const obj = parse(
+      'notifications { relayStatistics { usersCount: 101 broadcast { sessions { sessionId: "8tk6a9iiye" } network { outputKbps: 0 averageUser { outputKbps: 0 skipKbps: 0 } } } } }',
+      'new');
+    expect(obj.notifications.relayStatistics.broadcast.network.outputKbps).to.eq('0');
+  });
+
+  it('parse real log 2', () => {
+    const obj = parse(
+      'notifications { relayStatistics { usersCount: 1458 broadcast { sessions { sessionId: "tnrumne4t2" streams { streamId: "a89afd1b-d4d9-42f8-8803-b29c40be6be6" layers { mediaLayer { mediaType: Video bitrate: 100000 framerate: 15 } targetsCount: 181 } layers { mediaLayer { mediaType: Video bitrate: 500000 framerate: 15 } targetsCount: 151 } layers { mediaLayer { mediaType: Video bitrate: 1000000 framerate: 15 } targetsCount: 323 } layers { mediaLayer { mediaType: Video bitrate: 300000 framerate: 15 } targetsCount: 123 } layers { mediaLayer { mediaType: Video bitrate: 1500000 framerate: 15 } targetsCount: 192 } layers { mediaLayer { mediaType: Video bitrate: 1200000 framerate: 15 } targetsCount: 238 } layers { mediaLayer { mediaType: Video bitrate: 700000 framerate: 15 } targetsCount: 226 } } } network { outputKbps: 728768 averageUser { outputKbps: 504 skipKbps: 0 } } } } }',
+      'new');
+    expect(obj.notifications.relayStatistics.usersCount).to.eq('1458');
+    expect(obj.notifications.relayStatistics.broadcast.sessions.sessionId).to.eq('tnrumne4t2');
+    expect(obj.notifications.relayStatistics.broadcast.sessions.streams.layers.length).to.eq(7);
+  });
+
+  it('parse real log record', () => {
+    const obj = parse('InboundNotifications(notifications=Notifications(notifications={Notification(content=RelayStatistics(value=RelayStatistics(usersCount=50, broadcast=Broadcast(sessions={Session(sessionId=o5rpqlivyz, streams={Stream(streamId=BF1DB1B4-F710-4E83-9D5D-1D799E57AD38, layers={Layer(mediaLayer=Video 1000kbps, targetsCount=50)})})}, network=Some(Network(outputKbps=20719, averageUser=AverageUser(outputKbps=414, skipKbps=0)))))))}))');
+    expect(obj.notifications.notifications[0].content.value).not.null;
+  });
+
+  it('parse simple log where value has space', () => {
+    const obj = parse('Car(brand=BMW X5)');
+    expect(obj.brand).to.eq('BMWX5');
+  });
+
   it('parse log with same fields', () => {
     const obj = parse('Car(brand=BMW, year=4)');
     expect(obj.brand).to.eq('BMW');
